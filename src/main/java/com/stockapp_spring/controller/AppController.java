ca9788273974798e03b30ba66072e9efc1ef2600
Kristof910 <kristof910@icloud.com>
2020-10-29 16:10:00 +0100

    spring version (not final)

---
+++ src/main/java/com/stockapp_spring/controller/AppController.java
@@ -0,0 +1,32 @@
+package com.stockapp_spring.controller;
+
+import com.stockapp_spring.service.Trader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @Autowired
+    private Trader trader;
+
+    @PostMapping("/buy/{stock}/{price}")
+    public String start(@PathVariable("stock") String symbol, @PathVariable("price") int price) {
+        String content = "";
+        try {
+            boolean purchased = trader.buy(symbol, price);
+            if (purchased) {
+                content += "Purchased stock!";
+            }
+            else {
+                content += "Couldn't buy the stock at that price.";
+            }
+        } catch (Exception e) {
+            content += "There was an error while attempting to buy the stock: " + e.getMessage();
+        }
+        return content;
+    }
+
+}
